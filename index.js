618a6b4050b870cc07483c2918726fcd6b862c36
msn-05 <68454203+msn-05@users.noreply.github.com>
2021-02-27 15:33:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ function elem(id){
     return document.getElementById(id);
 }
 window.onload = () => {
-    alert("This game was made by MSN(Md Saadman Nuheen) in Sololearn");
-    alert("You're probably thinking what is disArmageddon");
-    alert("It means u have to stop a bomb!!!");
-    alert("U have 5 seconds to disarm the bomb");
+    Swal.fire("Intro","This game was made by MSN(Md Saadman Nuheen) in Sololearn","info");
+    Swal.fire({text:"You have 5 seconds to disarm the bomb",icon:"warning});
     var p = elem("time");
     var int;
     var min = 0;
@@ -30,15 +28,14 @@ document.body.removeChild(elem("cont"));
            elem("explode").style.display = "block";
            navigator.vibrate(700);
            setTimeout(function(){
-               alert("Boom!");
+               Swal.fire("Boom!","You failed to stop the bomb","error");
            },1000);}
         },10);
         elem("b").onclick = function(){
                 elem("b").style.backgroundColor = "red";
                 elem("b").style.color = "black";
                 clearInterval(int);
-                alert("Congralutions!!!\nU stopped the bomb!");
-                alert("That was easy ,wasn't it?");
+                Swal.fire("Congralutions!","You stopped the bomb!","success");
                 document.body.removeChild(elem("cont"));            
                 elem("congo").style.display = "block";
             };
